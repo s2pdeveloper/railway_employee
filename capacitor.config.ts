27dcbc3b5712eb854373b2ea886bb7829eb6608e
pratikme14123 <pratikbangadkar@s2pedutech.com>
2024-04-10 11:16:56 +0530

    chnages

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'railway-employee',
+  appId: 'com.ress.app',
+  appName: 'RESS',
   webDir: 'www',
   server: {
     androidScheme: 'https'
