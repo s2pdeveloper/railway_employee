44ffa5e1d296d12ff06b84f202e461fb1b90fb1b
Ayush849 <ayushamnerkar9@gmail.com>
2024-05-04 17:26:00 +0530

    changes done

--- src/app/features/pages/salary-view/components/monthly-salary/monthly-salary.component.ts
+++ src/app/features/pages/salary-view/components/monthly-salary/monthly-salary.component.ts
@@ -11,6 +11,8 @@ export class MonthlySalaryComponent implements OnInit {
   salaryData: any;
   loanECCSBPLS: number = 0;
   loanECCSBPLL: number = 0;
+  loanECCSBWKS: number = 0;
+  loanECCSWKL: number = 0;
   constructor() {
     this.salaryData = this.storageService.get('SalaryData');
   }
@@ -18,5 +20,7 @@ export class MonthlySalaryComponent implements OnInit {
   ngOnInit() {
     this.loanECCSBPLS = this.salaryData?.loanECCSBPL.split(' ')[0];
     this.loanECCSBPLL = this.salaryData?.loanECCSBPL.split(' ')[1];
+    this.loanECCSBWKS = this.salaryData?.loanECCSBWK.split(' ')[0];
+    this.loanECCSWKL = this.salaryData?.loanECCSBWK.split(' ')[1];
   }
 }
